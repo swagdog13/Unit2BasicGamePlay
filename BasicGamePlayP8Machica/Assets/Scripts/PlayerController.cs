aff4e9d9857e3f5f43004973ddc9fd19b5b1f658
swagdog13 <727159@student.dusd.net>
2025-03-24 15:24:06 -0700

    New
    I added a new script and made food a prefab

--- BasicGamePlayP8Machica/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayP8Machica/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
