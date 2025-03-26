84cfbd10200c02aaef219119be8c6c2a0789c026
swagdog13 <727159@student.dusd.net>
2025-03-26 15:28:42 -0700

    New features
    the animals can move now and the food is now a projecti;e

--- BasicGamePlayP8Machica/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayP8Machica/Assets/Scripts/PlayerController.cs
@@ -22,9 +22,15 @@ public class PlayerController : MonoBehaviour
         //keep the player inbounds
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
