8845c652c19d48ab8f81574b23d1ff6141f0a872
swagdog13 <727159@student.dusd.net>
2025-04-08 14:44:02 -0700

    UI
    I added UI to myt player

--- BasicGamePlayP8Machica/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGamePlayP8Machica/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,11 +7,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     private float topBound = 30;
     private float lowerBound = -10;
     private float sideBound = 30;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -22,17 +23,17 @@ public class DestroyOutOfBounds : MonoBehaviour
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if (transform.position.x > sideBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if(transform.position.x < -sideBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
     }
--- BasicGamePlayP8Machica/Assets/Scripts/DetectColllisions.cs
+++ BasicGamePlayP8Machica/Assets/Scripts/DetectColllisions.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DetectColllisions : MonoBehaviour
 {
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -19,11 +20,12 @@ public class DetectColllisions : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
-        else
+        else if (other.CompareTag("Animal"))
         {
+            gameManager.AddScore(5);
             Destroy(gameObject);
             Destroy(other.gameObject);
         }
